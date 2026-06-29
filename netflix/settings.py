7c5e8220d2810b2a03fe96a4d83d86e0069dcd05
nadirovabdulvohid3-max <nadirovabdulvohid3@gmail.com>
2026-06-29 23:16:12 +0500

    Update settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -90,9 +90,10 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
